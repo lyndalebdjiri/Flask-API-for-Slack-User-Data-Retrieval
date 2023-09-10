02e0df6daf805aa2aaf54bc6acf14f5614ce26dc
Lynda Lebdjiri <94696703+lyndalebdjiri@users.noreply.github.com>
2023-09-10 11:45:00 +0100

    Delete Backend/form.py

--- Backend/form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class Form_validation(FlaskForm):
-    slack_name = StringField("Slack_Name", validators=[DataRequired()])
-    track = StringField("Track", validators=[DataRequired()])
